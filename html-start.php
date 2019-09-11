9d0f619bd9c8b6545330802fc92bea850f10cccd
Albert <otec-akk@mail.ru>
2019-09-11 16:07:26 +0400

    добавил фавикон

--- html-start.php
+++ html-start.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/styles.css">
 	<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.min.css">
@@ -10,7 +11,7 @@
 	<script src="js/jquery-3.3.1.min.js"></script>
 	<script src="https://api-maps.yandex.ru/2.1/?apikey=bb5a6aff-2144-414d-87dc-ed5295dd426a
 &lang=ru_RU" type="text/javascript"></script>
-	<title>Картоха</title>
+	<title>РусУвроплант</title>
 </head>
 <body>
 	<header>
